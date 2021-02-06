313726e324ab0bdd74cd20064e97aa44af2da18b
Lucas Moeller <lucasmoeller@Lucass-MacBook-Air.local>
2021-02-06 16:29:51 +0100

    improved lstm

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -1,28 +1,47 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import relu, sigmoid
 
 
 class LSTM(nn.Module):
 
     def __init__(self, in_dim, hidden_dim, n_classes, n_layers=1, p_drop=.5):
         super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(
+        self.lstm1 = nn.LSTM(
             input_size=in_dim,
             hidden_size=hidden_dim,
             num_layers=n_layers,
             batch_first=True
         )
-        self.drop = nn.Dropout(p=p_drop)
-        self.fc = nn.Linear(
+        self.drop1 = nn.Dropout(p=p_drop)
+        self.lstm2 = nn.LSTM(
+            input_size=hidden_dim,
+            hidden_size=hidden_dim,
+            num_layers=n_layers,
+            batch_first=True
+        )
+        self.drop2 = nn.Dropout(p=p_drop)
+        self.fc1 = nn.Linear(
+            in_features=hidden_dim,
+            out_features=hidden_dim
+        )
+        self.bn = nn.BatchNorm1d(hidden_dim)
+        self.drop3 = nn.Dropout(p=p_drop)
+        self.fc2 = nn.Linear(
             in_features=hidden_dim,
             out_features=n_classes
         )
 
     def forward(self, x):
         x = x.transpose(-1, -2)
-        x, _ = self.lstm(x)
+        x, _ = self.lstm1(x)
+        x = self.drop1(x)
+        x, _ = self.lstm2(x)
+        x = self.drop2(x)
         x = torch.mean(x, dim=1).view(x.shape[0], -1)
         # x = x[:, -1].view(x.shape[0], -1)
-        x = self.drop(x)
-        x = self.fc(x)
+        x = self.fc1(x)
+        x = relu(self.bn(x))
+        x = self.drop2(x)
+        x = sigmoid(self.fc2(x))
         return x
